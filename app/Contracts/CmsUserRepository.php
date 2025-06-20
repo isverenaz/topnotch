4516d2d8af190e5bca7d9c8fbf50f8eda76179f3
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-06-20 12:44:02 +0400

    bugfix

--- app/Contracts/CmsUserRepository.php
+++ app/Contracts/CmsUserRepository.php
@@ -6,7 +6,6 @@ interface CmsUserRepository
 {
     public function getAll();
     public function create(array $data);
-    public function edit($id);
     public function update($id, array $data);
     public function delete($id);
 }
