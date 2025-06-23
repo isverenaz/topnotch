0eddfdc79776bba5df8173c79c5841e2eafff1a0
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-06-24 00:48:56 +0400

    bugfix

--- resources/lang/az/site.php
+++ resources/lang/az/site.php
@@ -62,7 +62,7 @@
     //reklam
     'header_title' => 'Dostun ilə gəl <a href="#">Xaricdə təhsil üçün</a> 10% endirim qazan!',
     'home_title_one' => 'İELTS üçün kurs axtarırsan?',
-    'home_text_one' => 'Ele indi <span>yay kampaniyalarımızdan</span> faydalan.',
+    'home_text_one' => 'Ele indi yay kampaniyalarımızdan faydalan.',
     'home_title_two' => 'Fransız dili kursu axtarırsan?',
     'home_text_two' => 'TƏLƏS bizim tədris üçün.',
     'about_title' => 'Become A Instructor',
