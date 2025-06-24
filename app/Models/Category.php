fc96a1716e17c4d13cc9d7e1a2c296373f21f85e
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-06-24 12:43:19 +0400

    Bugfix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -26,6 +26,6 @@ class Category extends Model
 
     public function news()
     {
-        return $this->hasMany(News::class,'category_id','id')->where(['status' => 1])->orderBy('datetime','DESC');
+        return $this->hasMany(News::class,'category_id','id')->where(['status' => 1, 'is_main' => 1])->orderBy('datetime','DESC');
     }
 }
