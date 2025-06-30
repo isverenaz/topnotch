698868640a15a53e1da8efcc61cc2f2e40d73963
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-06-30 17:01:19 +0400

    bugfix

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
@@ -48,47 +49,55 @@ class HomeController extends Controller
 
     public function __construct()
     {
-        $this->currentLang = LaravelLocalization::getCurrentLocale();
-        $locales = Translation::where('status',1)->pluck('code')->toArray();
-        if (!in_array($this->currentLang,$locales)){
-            return self::notFound();
-        }
+        $this->middleware(function ($request, $next) {
+            $this->currentLang = LaravelLocalization::getCurrentLocale();
+            $locales = Translation::where('status', 1)->pluck('code')->toArray();
+            if (!in_array($this->currentLang, $locales)) {
+                return self::notFound();
+            }
+
+            if ($request->path() === '/') {
+                return redirect('/az');
+            }
+
+            return $next($request);
+        });
     }
 
     public function index()
     {
         $currentLang = $this->currentLang;
-        $slider = Slider::where('status',1)->orderBy('id','DESC')->first();
-        $categories = Category::where(['status' => 1])->orderBy('id','DESC')->get();
-        $studyAbroads = StudyAbroad::where(['status' => 1, 'is_main' => 1])->orderBy('id','DESC')->paginate(3);
-        $languageCourses = LanguageCourse::where(['status' => 1, 'is_main' => 1])->orderBy('id','DESC')->paginate(3);
-        $universities = University::whereNotNull('image')->where(['status' => 1])->orderBy('id','DESC')->get();
-        $services = Service::where(['status' => 1])->orderBy('id','DESC')->get();
-        return view('site.home',compact('currentLang','slider','categories','studyAbroads','languageCourses','universities','services'));
+        $slider = Slider::where('status', 1)->orderBy('id', 'DESC')->first();
+        $categories = Category::where(['status' => 1])->orderBy('id', 'DESC')->get();
+        $studyAbroads = StudyAbroad::where(['status' => 1, 'is_main' => 1])->orderBy('id', 'DESC')->paginate(3);
+        $languageCourses = LanguageCourse::where(['status' => 1, 'is_main' => 1])->orderBy('id', 'DESC')->paginate(3);
+        $universities = University::whereNotNull('image')->where(['status' => 1])->orderBy('id', 'DESC')->get();
+        $services = Service::where(['status' => 1])->orderBy('id', 'DESC')->get();
+        return view('site.home', compact('currentLang', 'slider', 'categories', 'studyAbroads', 'languageCourses', 'universities', 'services'));
     }
 
     public function studyAbroad($country = null, $university = null)
     {
         $currentLang = $this->currentLang;
         if ($country != null && $university != null) {
-            $country = Country::where(['slug->'.$currentLang => $country, 'status' => 1])->first();
-            $university = University::where(['slug->'.$currentLang => $university, 'status' => 1])->first();
-            $studyAbroads = StudyAbroad::where(['country_id' => $country->id, 'university_id' => $university->id, 'status' => 1])->orderBy('id','DESC')->get();
-        }elseif ($country != null && $university == null) {
-            $country = Country::where(['slug->'.$currentLang => $country, 'status' => 1])->first();
-            $studyAbroads = StudyAbroad::where(['country_id' => $country->id, 'status' => 1])->orderBy('id','DESC')->get();
-        }else {
-            $studyAbroads = StudyAbroad::where(['status' => 1])->orderBy('id','DESC')->get();
+            $country = Country::where(['slug->' . $currentLang => $country, 'status' => 1])->first();
+            $university = University::where(['slug->' . $currentLang => $university, 'status' => 1])->first();
+            $studyAbroads = StudyAbroad::where(['country_id' => $country->id, 'university_id' => $university->id, 'status' => 1])->orderBy('id', 'DESC')->get();
+        } elseif ($country != null && $university == null) {
+            $country = Country::where(['slug->' . $currentLang => $country, 'status' => 1])->first();
+            $studyAbroads = StudyAbroad::where(['country_id' => $country->id, 'status' => 1])->orderBy('id', 'DESC')->get();
+        } else {
+            $studyAbroads = StudyAbroad::where(['status' => 1])->orderBy('id', 'DESC')->get();
         }
-        return view('site.study-abroad',compact('currentLang','country','university','studyAbroads'));
+        return view('site.study-abroad', compact('currentLang', 'country', 'university', 'studyAbroads'));
     }
 
     public function degreeStudyAbroad($degree = null)
     {
         $currentLang = $this->currentLang;
-        $degree = EducationalDegree::where(['slug->'.$currentLang => $degree, 'status' => 1])->first();
-        $studyAbroads = StudyAbroad::where(['degree_id' => $degree->id, 'status' => 1])->orderBy('id','DESC')->get();
-        return view('site.degree-study-abroad',compact('currentLang','degree','studyAbroads'));
+        $degree = EducationalDegree::where(['slug->' . $currentLang => $degree, 'status' => 1])->first();
+        $studyAbroads = StudyAbroad::where(['degree_id' => $degree->id, 'status' => 1])->orderBy('id', 'DESC')->get();
+        return view('site.degree-study-abroad', compact('currentLang', 'degree', 'studyAbroads'));
     }
 
     public function studyAbroadDetails($country = null, $university = null, $slug)
@@ -97,26 +106,26 @@ class HomeController extends Controller
         if ($country != null && $university != null) {
         }
 
-        $country = Country::where(['slug->'.$currentLang => $country, 'status' => 1])->first();
-        $university = University::where(['slug->'.$currentLang => $university, 'status' => 1])->first();
-        $studyAbroad = StudyAbroad::where(['country_id' => $country->id, 'university_id' => $university->id, 'slug->'.$currentLang =>$slug, 'status' => 1])->orderBy('id','DESC')->first();
-        return view('site.study-abroad-details',compact('currentLang','country','university','studyAbroad'));
+        $country = Country::where(['slug->' . $currentLang => $country, 'status' => 1])->first();
+        $university = University::where(['slug->' . $currentLang => $university, 'status' => 1])->first();
+        $studyAbroad = StudyAbroad::where(['country_id' => $country->id, 'university_id' => $university->id, 'slug->' . $currentLang => $slug, 'status' => 1])->orderBy('id', 'DESC')->first();
+        return view('site.study-abroad-details', compact('currentLang', 'country', 'university', 'studyAbroad'));
     }
 
     public function languageCourses($language = null, $leve = null)
     {
         $currentLang = $this->currentLang;
         if ($language != null && $leve != null) {
-            $language = Language::whereNull('parent_id')->where(['slug->'.$currentLang => $language, 'status' => 1])->first();
-            $leve = Language::whereNotNull('parent_id')->where(['parent_id'=> $language['id'], 'slug->'.$currentLang => $leve, 'status' => 1])->first();
-            $languageCourse = LanguageCourse::where(['language_id' => $language->id, 'parent_language_id' => $leve->id, 'status' => 1])->orderBy('id','DESC')->get();
-        }elseif ($language != null && $leve == null) {
-            $language = Language::whereNull('parent_id')->where(['slug->'.$currentLang => $language, 'status' => 1])->first();
-            $languageCourse = LanguageCourse::where(['language_id' => $language->id, 'status' => 1])->orderBy('id','DESC')->get();
-        }else {
-            $languageCourse = LanguageCourse::where(['status' => 1])->orderBy('id','DESC')->get();
+            $language = Language::whereNull('parent_id')->where(['slug->' . $currentLang => $language, 'status' => 1])->first();
+            $leve = Language::whereNotNull('parent_id')->where(['parent_id' => $language['id'], 'slug->' . $currentLang => $leve, 'status' => 1])->first();
+            $languageCourse = LanguageCourse::where(['language_id' => $language->id, 'parent_language_id' => $leve->id, 'status' => 1])->orderBy('id', 'DESC')->get();
+        } elseif ($language != null && $leve == null) {
+            $language = Language::whereNull('parent_id')->where(['slug->' . $currentLang => $language, 'status' => 1])->first();
+            $languageCourse = LanguageCourse::where(['language_id' => $language->id, 'status' => 1])->orderBy('id', 'DESC')->get();
+        } else {
+            $languageCourse = LanguageCourse::where(['status' => 1])->orderBy('id', 'DESC')->get();
         }
-        return view('site.language-courses',compact('currentLang','language','languageCourse','leve'));
+        return view('site.language-courses', compact('currentLang', 'language', 'languageCourse', 'leve'));
     }
 
 
@@ -125,106 +134,116 @@ class HomeController extends Controller
         $currentLang = $this->currentLang;
         if ($language == null && $leve == null) {
         }
-        $language = Language::whereNull('parent_id')->where(['slug->'.$currentLang => $language, 'status' => 1])->first();
-        $leve = Language::whereNotNull('parent_id')->where(['parent_id'=> $language['id'], 'slug->'.$currentLang => $leve, 'status' => 1])->first();
-        $languageCourse = LanguageCourse::where(['language_id' => $language->id, 'parent_language_id' => $leve->id, 'status' => 1])->orderBy('id','DESC')->first();
-        return view('site.language-courses-details',compact('currentLang','language','languageCourse','leve'));
+        $language = Language::whereNull('parent_id')->where(['slug->' . $currentLang => $language, 'status' => 1])->first();
+        $leve = Language::whereNotNull('parent_id')->where(['parent_id' => $language['id'], 'slug->' . $currentLang => $leve, 'status' => 1])->first();
+        $languageCourse = LanguageCourse::where(['language_id' => $language->id, 'parent_language_id' => $leve->id, 'status' => 1])->orderBy('id', 'DESC')->first();
+        return view('site.language-courses-details', compact('currentLang', 'language', 'languageCourse', 'leve'));
     }
 
     public function signup()
     {
         return redirect(route('site.contact'));
         $currentLang = $this->currentLang;
-        return view('site.signup',compact('currentLang'));
+        return view('site.signup', compact('currentLang'));
     }
 
     public function projectDetail($slug)
     {
         $currentLang = $this->currentLang;
-        return view('site.project-detail',compact('currentLang'));
+        return view('site.project-detail', compact('currentLang'));
     }
 
     public function serviceDetail($slug)
     {
         $currentLang = $this->currentLang;
-        return view('site.service-detail',compact('currentLang'));
+        return view('site.service-detail', compact('currentLang'));
     }
 
     public function blogs($category = null)
     {
         $currentLang = $this->currentLang;
         if ($category != null) {
-            $category = Category::where(['slug->'.$currentLang => $category, 'status' => 1])->first();
-            $blogs = News::where(['category_id'=>$category->id,'status' => 1])->orderBy('id','DESC')->get();
-        }else{
-            $blogs = News::where(['status' => 1])->orderBy('id','DESC')->get();
+            $category = Category::where(['slug->' . $currentLang => $category, 'status' => 1])->first();
+            $blogs = News::where(['category_id' => $category->id, 'status' => 1])->orderBy('id', 'DESC')->get();
+        } else {
+            $blogs = News::where(['status' => 1])->orderBy('id', 'DESC')->get();
         }
-        return view('site.blogs',compact('currentLang','category','blogs'));
+        return view('site.blogs', compact('currentLang', 'category', 'blogs'));
     }
 
 
-    public function blogDetail($category = null,$slug)
+    public function blogDetail($category = null, $slug)
     {
         $currentLang = $this->currentLang;
-        $category = Category::where(['slug->'.$currentLang => $category, 'status' => 1])->first();
-        $blog = News::where(['category_id'=>$category->id,'slug->'.$currentLang => $slug,'status' => 1])->orderBy('id','DESC')->first();
-        return view('site.blog-detail',compact('currentLang','category','blog'));
+        $category = Category::where(['slug->' . $currentLang => $category, 'status' => 1])->first();
+        $blog = News::where(['category_id' => $category->id, 'slug->' . $currentLang => $slug, 'status' => 1])->orderBy('id', 'DESC')->first();
+        return view('site.blog-detail', compact('currentLang', 'category', 'blog'));
     }
+
     public function schools($schoolCategory = null)
     {
         $currentLang = $this->currentLang;
         if ($schoolCategory != null) {
-            $schoolCategory = SchoolCategory::where(['slug->'.$currentLang => $schoolCategory, 'status' => 1])->first();
-            $schools = School::with(['category', 'language', 'parentLanguage','teacher','country'])->where(['category_id'=>$schoolCategory->id,'status' => 1])->orderBy('id','DESC')->get();
-        }else{
-            $schools = School::with(['category', 'language', 'parentLanguage','teacher','country'])->where(['status' => 1])->orderBy('id','DESC')->get();
+            $schoolCategory = SchoolCategory::where(['slug->' . $currentLang => $schoolCategory, 'status' => 1])->first();
+            $schools = School::with(['category', 'language', 'parentLanguage', 'teacher', 'country'])->where(['category_id' => $schoolCategory->id, 'status' => 1])->orderBy('id', 'DESC')->get();
+        } else {
+            $schools = School::with(['category', 'language', 'parentLanguage', 'teacher', 'country'])->where(['status' => 1])->orderBy('id', 'DESC')->get();
         }
-        return view('site.schools',compact('currentLang','schoolCategory','schools'));
+        return view('site.schools', compact('currentLang', 'schoolCategory', 'schools'));
     }
 
 
-    public function schoolDetails($schoolCategory = null,$slug)
+    public function schoolDetails($schoolCategory = null, $slug)
     {
         $currentLang = $this->currentLang;
-        $schoolCategory = SchoolCategory::where(['slug->'.$currentLang => $schoolCategory, 'status' => 1])->first();
-        $school = School::with(['category', 'language', 'parentLanguage','teacher','country'])->where(['category_id'=>$schoolCategory->id,'slug->'.$currentLang => $slug,'status' => 1])->orderBy('id','DESC')->first();
-        return view('site.school-detail',compact('currentLang','schoolCategory','school'));
+        $schoolCategory = SchoolCategory::where(['slug->' . $currentLang => $schoolCategory, 'status' => 1])->first();
+        $school = School::with(['category', 'language', 'parentLanguage', 'teacher', 'country'])->where(['category_id' => $schoolCategory->id, 'slug->' . $currentLang => $slug, 'status' => 1])->orderBy('id', 'DESC')->first();
+        return view('site.school-detail', compact('currentLang', 'schoolCategory', 'school'));
     }
 
 
-    public function contact() {
+    public function contact()
+    {
         $currentLang = $this->currentLang;
         $setting = Setting::first();
-        return view('site.contact',compact('currentLang','setting'));
+        return view('site.contact', compact('currentLang', 'setting'));
     }
 
-    public function about () {
+    public function about()
+    {
         $currentLang = $this->currentLang;
-        $teachers = Teacher::where(['status' => 1])->orderBy('id','DESC')->get();
-        return view('site.about ',compact('currentLang','teachers'));
+        $teachers = Teacher::where(['status' => 1])->orderBy('id', 'DESC')->get();
+        return view('site.about ', compact('currentLang', 'teachers'));
     }
-    public function faqs() {
+
+    public function faqs()
+    {
         $currentLang = $this->currentLang;
-        $faqs = Faq::orderBy('id','DESC')->get();
-        return view('site.faqs',compact('currentLang','faqs'));
+        $faqs = Faq::orderBy('id', 'DESC')->get();
+        return view('site.faqs', compact('currentLang', 'faqs'));
     }
-    public function conditions() {
+
+    public function conditions()
+    {
         $currentLang = $this->currentLang;
-        return view('site.conditions',compact('currentLang'));
+        return view('site.conditions', compact('currentLang'));
     }
-    public function rules() {
+
+    public function rules()
+    {
         $currentLang = $this->currentLang;
-        return view('site.rules',compact('currentLang'));
+        return view('site.rules', compact('currentLang'));
     }
+
     public function notPage()
     {
         $currentLang = $this->currentLang;
-        return view('errors.404',compact('currentLang'));
+        return view('errors.404', compact('currentLang'));
     }
 
     public function notFound()
     {
         $currentLang = $this->currentLang;
-        return view('site.not_found',compact('currentLang'));
+        return view('site.not_found', compact('currentLang'));
     }
 }
