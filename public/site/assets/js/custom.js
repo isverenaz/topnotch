298599c7e18faa2eb2fb7679c87ea251fb5580d9
Esgerov Enver <enver.esgerov0106@gmail.com>
2026-07-31 12:20:00 +0400

    add

--- public/site/assets/js/custom.js
+++ public/site/assets/js/custom.js
@@ -4,8 +4,8 @@ $(function() {
 	const siteI18n = window.siteI18n || {};
 	const slowAutoplaySpeed = 6500;
 	const slowTransitionSpeed = 1000;
-	const fastAutoplaySpeed = 2400;
-	const fastTransitionSpeed = 700;
+	const fastAutoplaySpeed = 1400;
+	const fastTransitionSpeed = 500;
 	const slickArrow = {
 	  prevArrow: '<button type="button" class="slick-prev" aria-label="' + (siteI18n.previousSlide || 'Previous slide') + '"><i class="bi bi-chevron-left" aria-hidden="true"></i></button>',
 	  nextArrow: '<button type="button" class="slick-next" aria-label="' + (siteI18n.nextSlide || 'Next slide') + '"><i class="bi bi-chevron-right" aria-hidden="true"></i></button>'
